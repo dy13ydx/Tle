e5fae0767210f7984d900cdebb19a5fd2aff3026
Dy13yDx <42460187+dy13ydx@users.noreply.github.com>
2025-11-23 15:49:49 +0700

    Update werkzeug2hashcat.py

--- Script/werkzeug2hashcat.py
+++ Script/werkzeug2hashcat.py
@@ -1,21 +1,55 @@
 #!/usr/bin/env python3
-
 import base64
 import codecs
 import re
-import sys
+import argparse
 
-if len(sys.argv) != 2:
-    print(f'usage: {sys.argv[0]} <werkzeug hash file>')
-    print('Input file has Werkzeug hashes one per line')
-    sys.exit(1)
+# Thanks https://github.com/hashcat/hashcat/issues/3205#issuecomment-2415236705
 
-with open(sys.argv[1], 'r') as f:
-    hashes = f.readlines()
-
-for h in hashes:
-    m = re.match(r'pbkdf2:sha256:(\d*)\$([^\$]*)\$(.*)', h)
-    iterations =  m.group(1)
+def process_hash(example):
+    m = re.match(br'pbkdf2:sha256:(\d*)\$([^\$]*)\$(.*)', example)
+    if not m:
+      print("Invalid hash format. Check for weird characters if you are supplying the hash directly from terminal.")
+      return None
+    
+    iterations = m.group(1)
     salt = m.group(2)
-    hashe = m.group(3)
-    print(f"sha256:{iterations}:{base64.b64encode(salt.encode()).decode()}:{base64.b64encode(codecs.decode(hashe,'hex')).decode()}")
+    main_hash = m.group(3)
+    
+    decoded_hash = codecs.decode(main_hash, 'hex')
+
+    return f"sha256:{iterations.decode()}:{base64.b64encode(salt).decode()}:{base64.b64encode(decoded_hash).decode()}"
+
+def main():
+    parser = argparse.ArgumentParser(description="Process PBKDF2 hash(es) from input and convert to applicable hashcat format.")
+    parser.add_argument('-s', '--hash', type=str, help="Single PBKDF2 Werkzeug hash to process.")
+    parser.add_argument('-l', '--hashlist', type=str, help="File containing PBKDF2 Werkzeug hashes to process, one per line.")
+    
+    args = parser.parse_args()
+
+    if args.hash:
+      hashcat_hash = process_hash(args.hash.encode())
+      print(hashcat_hash)
+      print(f"You can now crack this hash using: hashcat -m 10900 {hashcat_hash} /usr/share/wordlists/rockyou.txt")
+    elif args.hashlist:
+      hashlist = []
+      try:
+        with open(args.hashlist, 'r') as f:
+            for line in f:
+                line = line.strip()
+                if line:
+                  hashcat_hash = process_hash(line.encode())
+                  hashlist.append(hashcat_hash)
+                  print(hashcat_hash)
+        with open('werkzeug_converted_hashcat.hash', 'w') as hashout:
+            for hashcat in hashlist:
+              hashout.write(hashcat + "\n")
+        print(f"You can now crack this hashes using: hashcat -m 10900 werkzeug_converted_hashcat.hash /usr/share/wordlists/rockyou.txt")
+
+      except FileNotFoundError:
+          print(f"File {args.hashlist} not found.")
+    else:
+      print("Please provide either a single hash (-s) or a file containing hash each line (-l).")
+
+if __name__ == '__main__':
+  main()
