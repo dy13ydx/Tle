87b425d46811f4c4d5655ef1698f3c43d699d107
Dy13yDx <42460187+dy13ydx@users.noreply.github.com>
2026-05-07 12:39:36 +0700

    Update writable.ps1

--- Script/writable.ps1
+++ Script/writable.ps1
@@ -1,9 +1,14 @@
-Get-ChildItem $env:windir -Directory -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
+$target = Read-Host "Enter path to check"
+$id = [Security.Principal.WindowsIdentity]::GetCurrent()
+$myGroups = $id.Groups.Translate([Security.Principal.NTAccount]).Value
+$myGroups += $id.Name
+
+Get-ChildItem $target -Directory -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
+	if ([console]::KeyAvailable -and [console]::ReadKey($true).KeyChar -eq 'q') { break }
     $dir = $_
-    if ([console]::KeyAvailable -and [console]::ReadKey($true).KeyChar -eq 'q') { break }
     try {
         (Get-Acl $dir.FullName).Access | ForEach-Object {
-            if ($_.AccessControlType -eq "Allow" -and $_.IdentityReference.Value -eq "BUILTIN\Users") {
+            if ($_.AccessControlType -eq "Allow" -and $_.IdentityReference.Value -in $myGroups) {
                 if (($_.FileSystemRights -like "*Write*" -or $_.FileSystemRights -like "*Create*") -and $_.FileSystemRights -like "*Execute*") {
                     Write-Host "$($dir.FullName): $($_.IdentityReference.Value) ($($_.FileSystemRights))"
                 }
