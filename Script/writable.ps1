4a694910ac706087438805a6a60ae6f2c7b295d3
Dy13yDx <42460187+dy13ydx@users.noreply.github.com>
2026-05-19 14:50:22 +0700

    Update writable.ps1

--- Script/writable.ps1
+++ Script/writable.ps1
@@ -2,14 +2,15 @@ param([string]$target = "C:\inetpub")
 $id = [Security.Principal.WindowsIdentity]::GetCurrent()
 $myGroups = $id.Groups.Translate([Security.Principal.NTAccount]).Value + $id.Name
 
+$writeData = [System.Security.AccessControl.FileSystemRights]::WriteData
+
 Get-ChildItem $target -Directory -Recurse -ErrorAction SilentlyContinue | ForEach-Object {
     $dir = $_
     try {
         (Get-Acl $dir.FullName).Access | Where-Object { 
             $_.AccessControlType -eq "Allow" -and 
             $_.IdentityReference.Value -in $myGroups -and 
-            $_.FileSystemRights -match "Write|Create" -and 
-            $_.FileSystemRights -match "Execute" 
+            $_.FileSystemRights.HasFlag($writeData)
         } | ForEach-Object { 
             Write-Host "$($dir.FullName): $($_.IdentityReference.Value)" 
         }
